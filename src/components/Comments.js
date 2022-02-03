6d32d0b14f00e33fc5f4dc6da0d9022975a57b25
karsyn ansari <karsyn.ansari@gmail.com>
2022-02-03 11:06:24 -0700

    all deliverables met

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import userEvent from '@testing-library/user-event';
+import React, {useState} from 'react';
 
-function Comments() {
-  return <div>Comments</div>;
+function Comments({video}) {
+  const [count, setCount] = useState(0)
+
+  function handleLikes() {
+    setCount((count) => count +1)
+  }
+const commentDiv = video.comments.map((comment) => {
+  return (
+    <div>
+      <h4>{comment.user}</h4>
+      <p>{comment.comment}</p>
+      <button id={comment.id}onClick={handleLikes}>{count}</button>
+    </div>
+  )
+})
+  return <div>
+    <h3>2 Comments</h3>
+    {commentDiv}
+    </div>;
 }
 
 export default Comments;
--- src/components/MainContent.js
+++ src/components/MainContent.js
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Comments from "./Comments";
 
-function MainContent({video}) {
-  return <div>
-    <h2>{video.title}</h2>
-    <Comments />
-    </div>;
+function MainContent({ video }) {
+  const [likes, setLikes] = useState(video.upvotes);
+  const [downvotes, setDownvotes] = useState(video.downvotes);
+  const [isHideComments, setIsHideComments] = useState(false);
 
+  function handleUpvote() {
+    setLikes((likes) => likes + 1);
+  }
+  function handleDownvote() {
+    setDownvotes((downvotes) => downvotes - 1);
+  }
+  function handleComments() {
+    setIsHideComments(!isHideComments);
+  }
+  return (
+    <div>
+      <h2>{video.title}</h2>
+      <p>{`${video.views} Views | Uploaded ${video.createdAt}`}</p>
+      <button onClick={handleUpvote}>{`${likes}👍`}</button>
+      <button onClick={handleDownvote}>{`${downvotes}👎`}</button>
+
+      <div>
+        <button onClick={handleComments}>
+          {isHideComments ? "Hide Comments" : "Show Comments"}
+        </button>
+      </div>
+      <hr></hr>
+      {isHideComments ? <Comments video={video} />: null}
+    </div>
+  );
 }
 
 export default MainContent;
